1504fdeaed0661a48d0b9e72ad1bd5f0b95bebf2
Kevin Bryant <kevin.bryant@affinitycu.ca>
2025-05-06 15:00:49 -0600

    Check a range of array sizes to see if behaviour differs

--- VarBinaryTest.cs
+++ VarBinaryTest.cs
@@ -2,48 +2,104 @@ using System.Data;
 using Microsoft.Data.SqlClient;
 
 
-public static class Program {
-	const int Size = 20_000;
+public static class Program
+{
 
-	static byte[] data = Enumerable.Range(0, Size)
-		.Select(i => (byte)(i % 256))
-		.ToArray();
 
-	public static async Task Main(string[] args) {
-		try {
-			var connStr = args[0];
-			using var conn = new SqlConnection(connStr);
-			await conn.OpenAsync();
-			var cmd = conn.CreateCommand();
-			cmd.CommandText = """
+
+    public static async Task Main(string[] args)
+    {
+        try
+        {
+            var connStr = args[0];
+            using var conn = new SqlConnection(connStr);
+            await conn.OpenAsync();
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = """
 				IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'TestVarBinary')
 				BEGIN
 				    CREATE TABLE dbo.TestVarBinary (Id INT IDENTITY(1,1) PRIMARY KEY, Data VARBINARY(MAX));
 				END
 				""";
-			await cmd.ExecuteNonQueryAsync();
+            await cmd.ExecuteNonQueryAsync();
 
-			cmd.CommandText = """
+            var startSize = 0;
+            var currentSize = startSize;
+            var maxSize = 100_000;
+            var currentState = State.None;
+            var currentStateStartedAtSize = startSize;
+
+            while (currentSize <= maxSize)
+            {
+                byte[] data = Enumerable.Range(0, currentSize)
+                    .Select(i => (byte)(i % 256))
+                    .ToArray();
+
+                cmd.CommandText = """
 				IF NOT EXISTS (SELECT 1 FROM dbo.TestVarBinary)
 					INSERT INTO dbo.TestVarBinary (Data) VALUES (@data);
 				BEGIN
 					UPDATE dbo.TestVarBinary SET Data = @data;
 				END
-				""";	
-			cmd.Parameters.Add(new SqlParameter("@data", SqlDbType.VarBinary, Size) { Value = data });
-			await cmd.ExecuteNonQueryAsync();
+				""";
+                cmd.Parameters.Add(new SqlParameter("@data", SqlDbType.VarBinary, currentSize) { Value = data });
+                await cmd.ExecuteNonQueryAsync();
 
-			cmd.CommandText = "SELECT Data FROM dbo.TestVarBinary";
-			cmd.Parameters.Clear();
-			var result = (byte[])await cmd.ExecuteScalarAsync();
-			if (result.SequenceEqual(data)) {
-				Console.WriteLine("Data matches.");
-			} else {
-				Console.Error.WriteLine("Data mismatch.");
-			}
-		} catch (Exception ex) {
-			Console.Error.WriteLine(ex);
-		}
-	}
+                cmd.CommandText = "SELECT Data FROM dbo.TestVarBinary";
+                cmd.Parameters.Clear();
+                var result = (byte[])await cmd.ExecuteScalarAsync();
+                if (result.Length != data.Length)
+                {
+                    if (currentState != State.LengthMismatch)
+                    {
+                        WriteStateChange(currentSize, currentState, currentStateStartedAtSize);
+                        currentState = State.LengthMismatch;
+                        currentStateStartedAtSize = currentSize;
+                    }
+                    currentSize++;
+                }
+                else if (result.SequenceEqual(data))
+                {
+                    if (currentState != State.Match)
+                    {
+                        WriteStateChange(currentSize, currentState, currentStateStartedAtSize);
+                        currentState = State.Match;
+                        currentStateStartedAtSize = currentSize;
+                    }
+                    currentSize++;
+                }
+                else
+                {
+                    if (currentState != State.ContentMismatch)
+                    {
+                        WriteStateChange(currentSize, currentState, currentStateStartedAtSize);
+                        currentState = State.ContentMismatch;
+                        currentStateStartedAtSize = currentSize;
+                    }
+                    currentSize++;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex);
+        }
+    }
+
+    private static void WriteStateChange(int currentSize, State currentState, int currentStateStartedAtSize)
+    {
+        if (currentState != State.None)
+        {
+            Console.WriteLine($"{currentStateStartedAtSize} -> {currentSize - 1}\t({currentSize - currentStateStartedAtSize})\t{currentState.ToString()}");
+        }
+    }
+
+    public enum State
+    {
+        None,
+        Match,
+        LengthMismatch,
+        ContentMismatch
+    }
 }
 
